445065fa134510db11e375f512dc287c11a38888
lwrnce <lwrnceyuen@gmail.com>
2021-06-27 13:13:09 -0700

    update models & index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
-const Vote = require('./Vote');
 const Comment = require('./Comment');
 
 // create associations
@@ -22,30 +21,6 @@ User.belongsToMany(Post, {
   onDelete: 'SET NULL'
 });
 
-Post.belongsToMany(User, {
-  through: Vote,
-  as: 'voted_posts',
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-Vote.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: 'SET NULL'
-});
-
-Vote.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: 'SET NULL'
-});
-
-User.hasMany(Vote, {
-  foreignKey: 'user_id'
-});
-
-Post.hasMany(Vote, {
-  foreignKey: 'post_id'
-});
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
@@ -66,4 +41,4 @@ Post.hasMany(Comment, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote, Comment };
+module.exports = { User, Post, Comment };
